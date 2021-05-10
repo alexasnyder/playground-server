dba16a3ee905687d8ea10660d414cbc14cfe8a94
Alexa Snyder <asnyder@four51.com>
2021-05-10 16:46:16 -0500

    configure response headers

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.use('/testAPI', testAPIRouter);
 app.use('/message', messageRouter);
 app.use('/validaterecaptcha', validateRouter);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
